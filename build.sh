5e26b1cc0b79176c929ff22e2b94546251c30866
fmcejudo <fmcejudo@gmail.com>
2022-09-17 22:53:02 +0200

    feat: upgrade project to more recent versions

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME_8=~/.sdkman/candidates/java/8.0.242.hs-adpt
-export JAVA_HOME_14=~/.sdkman/candidates/java/14.0.0.hs-adpt
+export JAVA_HOME_11=~/.sdkman/candidates/java/11.0.11.hs-adpt
+export JAVA_HOME_17=~/.sdkman/candidates/java/17.0.1-tem
 mvn clean verify --global-toolchains toolchains.xml
